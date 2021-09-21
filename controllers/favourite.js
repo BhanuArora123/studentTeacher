3ff4f8eaf0217939a08efd9bfce4d597f4ed6f40
bhanu arora <mrbhanuarora@gmail.com>
2021-09-21 17:28:25 +0530

    successfully added

--- controllers/favourite.js
+++ controllers/favourite.js
@@ -1,111 +1,120 @@
 const studentModel = require("../models/students");
 const teacherModel = require("../models/teachers.js");
 const { validationResult } = require("express-validator");
-exports.addFavourite = (req,res,next) => {
+exports.addFavourite = (req, res, next) => {
     let fav_teacher = req.body.teacher;
     let studentData;
     studentModel.findOne({
-        email:req.email
+        email: req.email
     })
-    .then((stud) => {
-        if(!stud){
-            return res.status(404).json({
-                msg:"not found"
-            })
-        }
-        studentData = stud;
-        return teacherModel.findOne({
-            email:fav_teacher
-        })
-    })
-    .then((teacher) => {
-        console.log(teacher);
-        studentData.fav_teachers.push({
-            teacherId:teacher._doc._id
-        })
-        studentData.save();
-        teacher.students_liked.push({
-            studId:studentData._id
-        })
-        return teacher.save();
-    })
-    .then((teacherData) => {
-        return res.status(200).json({
-            msg:"teacher added to favourite successfully",
-            teacherData:{
-                ...teacherData._doc,password:undefined
+        .then((stud) => {
+            if (!stud) {
+                return res.status(404).json({
+                    msg: "not found"
+                })
             }
-        });
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+            studentData = stud;
+            return teacherModel.findOne({
+                email: fav_teacher
+            })
+        })
+        .then((teacher) => {
+            console.log(teacher);
+            studentData.fav_teachers.push({
+                teacherId: teacher._doc._id
+            })
+            studentData.save();
+            teacher.students_liked.push({
+                studId: studentData._id
+            })
+            return teacher.save();
+        })
+        .then((teacherData) => {
+            return res.status(200).json({
+                msg: "teacher added to favourite successfully",
+                teacherData: {
+                    ...teacherData._doc, password: undefined
+                }
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 }
-exports.mostFav = (req,res,next) => {
+exports.mostFav = (req, res, next) => {
     // using aggregations to find most favourite teacher
     teacherModel
-    .aggregate([{
-        $expr:{
-            $max:[
-                {
-                    $size:"$fav_teachers"
+        .aggregate([
+            {
+                $project: {
+                    name:"$name",
+                    arrayLength:{$size:"$students_liked"}
                 }
-            ]
-        }
-    }])
-    .then((val) => {
-        return res.status(200).json({
-            fav_teacher:{
-                ...val._doc,password:undefined
             }
+            ,
+            {
+                $sort:{
+                    arrayLength:-1
+                }
+            },
+            {
+                $limit:1
+            }
+            // { $lookup: {from: 'students', localField: 'students_liked.studId', foreignField: 'students_liked.studId', as: 'students_liked'} }
+        ])
+        .then((val) => {
+            return res.status(200).json({
+                fav_teacher: {
+                    ...val
+                }
+            })
         })
-    })
 }
-exports.removeTeacher = (req,res,next) => {
+exports.removeTeacher = (req, res, next) => {
     let teacherId = req.params.teacherId;
     console.log(teacherId);
     studentModel.findOne({
-        email:req.email
+        email: req.email
     })
-    .then((stud) => {
-        let modifiedData = stud.fav_teachers.filter((teacher) => {
-            if(teacher.teacherId.toString() != teacherId.toString()){
-                return teacher;
-            }
+        .then((stud) => {
+            let modifiedData = stud.fav_teachers.filter((teacher) => {
+                if (teacher.teacherId.toString() != teacherId.toString()) {
+                    return teacher;
+                }
+            })
+            console.log(modifiedData)
+            stud.fav_teachers = modifiedData;
+            return stud.save();
         })
-        console.log(modifiedData)
-        stud.fav_teachers = modifiedData;
-        return stud.save();
-    })
-    .then(() => {
-        return res.status(200).json({
-            msg:"deleted"
+        .then(() => {
+            return res.status(200).json({
+                msg: "deleted"
+            })
         })
-    })
 }
-exports.getTeacher = (req,res,next) => {
+exports.getTeacher = (req, res, next) => {
     teacherModel.find()
-    .then((arr) => {
-        let teacherArray = arr.map((teacher) => {
-            return {
-                ...teacher._doc,password:undefined
-            }
+        .then((arr) => {
+            let teacherArray = arr.map((teacher) => {
+                return {
+                    ...teacher._doc, password: undefined
+                }
+            })
+            return res.status(200).json({
+                teacherData: teacherArray
+            })
         })
-        return res.status(200).json({
-            teacherData:teacherArray
-        })
-    })
 }
-exports.favTeacher = (req,res,next) => {
+exports.favTeacher = (req, res, next) => {
     studentModel.findOne({
-        email:req.email
+        email: req.email
     })
-    .populate("fav_teachers.teacherId","name email _id")
-    .then((userData) => {
-        return res.status(200).json({
-            userData:{
-                ...userData._doc,password:undefined
-            }
+        .populate("fav_teachers.teacherId", "name email _id")
+        .then((userData) => {
+            return res.status(200).json({
+                userData: {
+                    ...userData._doc, password: undefined
+                }
+            })
         })
-    })
 }
